286d024c7029bde3e3aed90fc9dc4742eb8181e9
Nate Miller <natekmiller96@gmail.com>
2018-06-20 19:43:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 def starts_with_a_vowel?(word)
- if word.match(/b[aieou]\w*/) != nil
-   return true
- else
-   return false
- end
+  puts word.match(/b[aieou]\w*/)
+  if word.match(/b[aieou]\w*/) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
