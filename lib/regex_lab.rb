67244ca039bcafcb5439309b3fe21ab0a79f11f5
Nate Miller <natekmiller96@gmail.com>
2018-06-20 20:02:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Bing+\bun/)
+  an_array=text.scan(/\w*\Bing/)
+  return an_array.grep(/\bun\w*/)
 end
 
 def words_five_letters_long(text)
